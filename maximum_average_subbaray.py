9bd5b398478921eacd13d9e210054db2c636bfc2
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2024-07-21 17:43:10 -0700

    Create maximum_average_subbaray.py

---
+++ maximum_average_subbaray.py
@@ -0,0 +1,32 @@
+# Maximum Average Subarray
+# You are given an integer array nums consisting of n elements, and an integer k.
+# Find a contiguous subarray whose length is equal to k that has the maximum average 
+# value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+#
+# Example 1:
+# Input: nums = [1,12,-5,-6,50,3], k = 4
+# Output: 12.75000
+# Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+#
+# Example 2:
+# Input: nums = [5], k = 1
+# Output: 5.00000
+#
+# Constraints:
+#   - n == nums.length
+#   - 1 <= k <= n <= 105
+#   - 104 <= nums[i] <= 104
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr_sum = sum(nums[0:k])
+        max_sum = curr_sum
+        i = 1
+        while i < len(nums) - k + 1:
+            curr_sum = curr_sum - nums[i - 1] + nums[i + k - 1]
+            if curr_sum > max_sum :
+                max_sum = curr_sum
+            i += 1
+        
+        return max_sum/k
+
