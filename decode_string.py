9b173d4fb34be75a9e5ee4d50d5425e0a3fc3209
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2024-07-29 16:46:04 -0700

    Create decode_string.py

---
+++ decode_string.py
@@ -0,0 +1,53 @@
+# Decode String
+# Given an encoded string, return its decoded string.
+# The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+# You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+# The test cases are generated so that the length of the output will never exceed 105.
+#
+# Example 1:
+# Input: s = "3[a]2[bc]"
+# Output: "aaabcbc"
+#
+# Example 2:
+# Input: s = "3[a2[c]]"
+# Output: "accaccacc"
+#
+# Example 3:
+# Input: s = "2[abc]3[cd]ef"
+# Output: "abcabccdcdcdef"
+#
+# Constraints:
+#   - 1 <= s.length <= 30
+#   - s consists of lowercase English letters, digits, and square brackets '[]'.
+#   - s is guaranteed to be a valid input.
+#   - All the integers in s are in the range [1, 300].
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        i = 0
+        while i < len(s) :
+            # print(s[i])
+            if s[i] == ']' :
+                # get everything between [.....]
+                dec = ""
+                while len(stack) > 0 and stack[-1] != '[' :
+                    dec = stack.pop(len(stack) - 1) + dec
+                stack.pop(len(stack) - 1) # Remove the '['
+
+                # get the number before the [...]
+                num = ""
+                # print(stack)
+                while len(stack) > 0 and stack[-1].isnumeric() :
+                    num = stack.pop(len(stack) - 1) + num
+
+                # Find the multiplied string
+                new_str = ""
+                for k in range(int(num)) :
+                    new_str += dec
+                stack.append(new_str)
+            else :
+                stack.append(s[i])
+            i += 1
+
+        return ''.join(stack)
