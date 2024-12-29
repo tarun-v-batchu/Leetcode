de86a7c5e67d8e981159b930707761da6f989a81
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2024-12-28 18:52:39 -0800

    Create word_break.py

---
+++ word_break.py
@@ -0,0 +1,55 @@
+# Word Break
+
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into
+# a space-separated sequence of one or more dictionary words.
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+# Example 1:
+#
+# Input: s = "leetcode", wordDict = ["leet","code"]
+# Output: true
+#
+# Explanation: Return true because "leetcode" can be segmented as "leet code".
+#
+# Example 2:
+#
+# Input: s = "applepenapple", wordDict = ["apple","pen"]
+# Output: true
+#
+# Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+# Note that you are allowed to reuse a dictionary word.
+# 
+# Example 3:
+
+# Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+# Output: false
+ 
+
+# Constraints:
+
+#   - 1 <= s.length <= 300
+#   - 1 <= wordDict.length <= 1000
+#   - 1 <= wordDict[i].length <= 20
+#   - s and wordDict[i] consist of only lowercase English letters.
+#   - All the strings of wordDict are unique.
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        position_made = [False] * (len(s) + 1)
+        position_made[0] = True
+        i = 0
+        while i < len(s) :
+            if position_made[i] :
+                for word in wordDict :
+                    # print(s[i : i + len(word)], word)
+                    if i + len(word) < len(s) + 1 and s[i : i + len(word)] == word :
+                        position_made[i + len(word)] = True
+            # print(position_made)
+            i += 1
+
+        return position_made[-1] 
+
+
+
