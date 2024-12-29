5d0439043a2d3d08ebe5633785206e1c98b9e03c
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2024-12-28 19:38:42 -0800

    Create add_strings.py

---
+++ add_strings.py
@@ -0,0 +1,55 @@
+# Add Strings
+
+# Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+# You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+# Example 1:
+# Input: num1 = "11", num2 = "123"
+# Output: "134"
+
+# Example 2:
+# Input: num1 = "456", num2 = "77"
+# Output: "533"
+
+# Example 3:
+# Input: num1 = "0", num2 = "0"
+# Output: "0"
+ 
+
+# Constraints:
+#   - 1 <= num1.length, num2.length <= 104
+#   - num1 and num2 consist of only digits.
+#   - num1 and num2 don't have any leading zeros except for the zero itself.
+
+
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        
+        i = len(num1) - 1
+        j = len(num2) - 1
+        s = ""
+        carry = 0
+        while i >= 0 and j >= 0 :
+            dig = (int(num1[i]) + int(num2[j]) + carry) % 10
+            carry = (int(num1[i]) + int(num2[j]) + carry) // 10
+            s = str(dig) + s
+            i -= 1
+            j -= 1
+        
+        while i >= 0 :
+            dig = (int(num1[i]) + carry) % 10
+            carry = (int(num1[i]) + carry) // 10
+            s = str(dig) + s
+            i -= 1
+
+        while j >= 0 :
+            dig = (int(num2[j]) + carry) % 10
+            carry = (int(num2[j]) + carry) // 10
+            s = str(dig) + s
+            j -= 1
+        
+        if carry > 0 :
+            s = str(carry) + s
+
+        return s
+        
