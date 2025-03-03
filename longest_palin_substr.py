aa11b39568c44dc31974dcba7b9e9c60136efee3
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2025-03-03 12:54:48 -0800

    Update longest_palin_substr.py

--- longest_palin_substr.py
+++ longest_palin_substr.py
@@ -1,7 +1,5 @@
 # Longest Palindromic Substring
-
-# Given a string s, return the longest 
-# palindromic substring in s.
+# Given a string s, return the longest palindromic substring in s.
 
 # Example 1:
 # Input: s = "babad"
@@ -12,6 +10,7 @@
 # Input: s = "cbbd"
 # Output: "bb"
 
+
 # Constraints:
 #   - 1 <= s.length <= 1000
 #   - s consist of only digits and English letters.
@@ -20,28 +19,35 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        dp = [[0] * len(s) for _ in range(len(s))]
+        maxj = 0
+        maxk = 0
+
         i = 0
         while i < len(s) :
-            dp[i][i] = 1
+
+            j, k = i, i
+            while j >= 0 and k < len(s) and s[j] == s[k] :
+                # print(i, ":", s[j], s[k], s[j:k])
+                j -= 1
+                k += 1
+            
+            if maxk - (maxj + 1) < k - (j + 1) :
+                maxk = k
+                maxj = j
+            
+            j, k = i, i + 1
+            while j >= 0 and k < len(s) and s[j] == s[k] :
+                # print(i, ":", s[j], s[k], s[j:k])
+                j -= 1
+                k += 1
+            
+            if maxk - (maxj + 1) < k - (j + 1) :
+                maxk = k
+                maxj = j
+            
             i += 1
-
-        i_max = 0
-        j_max = 0
         
-        # print(dp)
-        for i in range(len(s)-1,-1,-1) :
-            for j in range(i + 1, len(s)) :
-                # print(s[i:j+1], s[i], ",", s[j], ":", (dp[i + 1][j - 1] + 2) if (s[i] == s[j] and dp[i+1][j-1] != 0) else 0, "smaller:", dp[i + 1][j - 1], s[i+1:j])
-                if s[i] == s[j] and (j - i == 1 or dp[i+1][j-1] != 0):
-                    
-                    dp[i][j] = j - i + 1
+        return s[maxj + 1:maxk]
+            
 
-                    if j_max - i_max +1 < j - i + 1 :
-                        i_max, j_max = i, j
-                # dp[i][j] = (dp[i + 1][j - 1] + 2) if (s[i] == s[j] and (dp[i+1][j-1] != 0 or i + 1 == j -1)) else 0
-
-        # print(dp)
-
-        return s[i_max:j_max+1]
 
