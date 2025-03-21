6269fb315a0a5aac22cb2f1470ee7da77a01d252
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2025-03-20 21:13:27 -0700

    Update longest_palin_substr.py

--- longest_palin_substr.py
+++ longest_palin_substr.py
@@ -46,8 +46,50 @@ class Solution:
                 maxj = j
             
             i += 1
-        
         return s[maxj + 1:maxk]
             
 
+# Attempt 2: 
+
+# Longest Palindromic Substring
+
+# Given a string s, return the longest substring of s that is a palindrome.
+# A palindrome is a string that reads the same forward and backward.
+# If there are multiple palindromic substrings that have the same length, return any one of them.
+
+# Example 1:
+# Input: s = "ababd"
+# Output: "bab"
+# Explanation: Both "aba" and "bab" are valid answers.
+
+# Example 2:
+# Input: s = "abbc"
+# Output: "bb"
+
+# Constraints:
+#   - 1 <= s.length <= 1000
+#   - s contains only digits and English letters.
+
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_str = s[0]
+        for k in range(len(s)) :
+            
+            i = k
+            j = k
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            max_str = max(s[i + 1:j], max_str, key=lambda x: len(x))
+            
+            i = k
+            j = k + 1
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            max_str = max(s[i + 1:j], max_str, key=lambda x: len(x))
+        
+        return max_str
 
