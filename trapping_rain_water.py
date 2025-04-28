0a72bb6e14707581b60ef12f9fc01209d475f397
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2025-04-27 21:45:51 -0400

    Update trapping_rain_water.py

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -48,3 +48,42 @@ class Solution:
 
         return area
 
+# Trapping Rain Water
+# Given n non-negative integers representing an elevation 
+# map where the width of each bar is 1, compute how much 
+# water it can trap after raining.
+
+# Example 1:
+# Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# Output: 6
+# Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+# Example 2:
+# Input: height = [4,2,0,3,2,5]
+# Output: 9
+
+# Constraints:
+#   - n == height.length
+#   - 1 <= n <= 2 * 104
+#   - 0 <= height[i] <= 105
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+
+        total, max_left, max_right = 0, 0, 0
+        while left < right :
+            if height[left] < height[right] :
+                max_left = max(max_left, height[left])
+                total += (max_left - height[left])
+                left += 1
+            else :
+                max_right = max(max_right, height[right])
+                total += (max_right - height[right])
+                right -= 1
+        return total
+
+
+
+
