d5622811db1fb1b6f7e5ab2a42b3be63a7aacd9d
tarun-v-batchu <135396607+tarun-v-batchu@users.noreply.github.com>
2025-05-04 16:27:32 -0400

    Create rotate_image.py

---
+++ rotate_image.py
@@ -0,0 +1,51 @@
+# Rotate Image
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees 
+# (clockwise).
+
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+# Example 1:
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+# Example 2:
+# Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+# Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ 
+# Constraints:
+#   - n == matrix.length == matrix[i].length
+#   - 1 <= n <= 20
+#   - -1000 <= matrix[i][j] <= 1000
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        def transpose(matrix) :
+
+            for i in range(len(matrix)) :
+                for j in range(i + 1, len(matrix)) :
+                    temp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = temp
+
+        def reflect(matrix) :
+
+            for row in range(len(matrix)) :
+                i, j = 0, len(matrix[row]) - 1
+                while i < j :
+                    temp = matrix[row][i]
+                    matrix[row][i] = matrix[row][j]
+                    matrix[row][j] = temp
+                    i += 1
+                    j -= 1
+        
+        transpose(matrix)
+        reflect(matrix)
+        return matrix
+
+
+
